8ffb5037eaab94547773fe350eee91b87ccd3b2c
a.sitnikov <timonsitnik@gmail.com>
2022-11-21 20:13:43 +0700

    application appearance

--- app/src/main/java/ru/eltex/phonebook/MainActivity.java
+++ app/src/main/java/ru/eltex/phonebook/MainActivity.java
@@ -3,6 +3,11 @@ package ru.eltex.phonebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView mainList = (ListView) findViewById(R.id.main_list);
+
+
+        User[] users = {
+                new User("Ivan", "700"),
+                new User("Mary", "800"),
+                new User("Dmitry", "900"),
+        };
+
+        PhoneAdapter phoneAdapter = new PhoneAdapter(this, users);
+        mainList.setAdapter(phoneAdapter);
+
+//        mainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                System.out.println(users[position].getName());
+//                Toast.makeText(getApplicationContext(), users[position].getPhone(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
+
 }
---
+++ app/src/main/java/ru/eltex/phonebook/PhoneAdapter.java
@@ -0,0 +1,43 @@
+package ru.eltex.phonebook;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class PhoneAdapter extends ArrayAdapter<User> {
+
+    private Context context;
+    private User[] users;
+
+    PhoneAdapter(Context context, User[] users){
+        super(context,R.layout.item, users);
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item, parent, false);
+
+        TextView name = (TextView) view.findViewById(R.id.name);
+        name.setText(this.users[position].getName());
+
+        TextView phone = (TextView) view.findViewById(R.id.phone);
+        phone.setText(this.users[position].getPhone());
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.avatar);
+        imageView.setImageResource(R.drawable.ic_launcher_foreground);
+
+        return view;
+    }
+}
