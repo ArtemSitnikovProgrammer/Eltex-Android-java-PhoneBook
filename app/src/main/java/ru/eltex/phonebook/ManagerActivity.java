0489c0143c926e1887f4f7656ade5197fa98cdd2
a.sitnikov <timonsitnik@gmail.com>
2022-11-28 00:26:20 +0700

    create activity developer and manager

--- app/src/main/java/ru/eltex/phonebook/ManagerActivity.java
+++ app/src/main/java/ru/eltex/phonebook/ManagerActivity.java
@@ -4,22 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DevActivity extends AppCompatActivity {
+public class ManagerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dev);
+        setContentView(R.layout.activity_manager);
 
         Intent info = getIntent();
         String name = info.getStringExtra("name");
         String phone = info.getStringExtra("phone");
         //String img = info.getStringExtra("avatar");
 
-        System.out.println("FROM DEVACTIVITY: " + name + " " + phone + " ");
+        System.out.println("FROM MANAGERACTIVITY: " + name + " " + phone + " ");
 
         //((ImageView) findViewById(R.id.avatar)).setImageResource();
         ((TextView) findViewById(R.id.name)).setText(name);
